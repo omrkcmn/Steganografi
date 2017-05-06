38172e2d01d212c4ed93263e017c3a5b8d402b1d
Ömer Kocaman <mer Kocaman>
2017-05-06 22:30:42 +0300

    Proje dosyası ekle.

---
+++ Steganografi/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Steganografi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.picIsleme = new System.Windows.Forms.PictureBox();
+            this.txtMesaj = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnCoz = new System.Windows.Forms.Button();
+            this.btnGizle = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnAc = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuKullanim = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuCikis = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuIletisim = new System.Windows.Forms.ToolStripMenuItem();
+            this.mneuHakkinda = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripSayi = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picIsleme)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.picIsleme);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.groupBox1.Location = new System.Drawing.Point(12, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(643, 359);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "İşlenecek Resim";
+            // 
+            // picIsleme
+            // 
+            this.picIsleme.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picIsleme.Location = new System.Drawing.Point(6, 19);
+            this.picIsleme.Name = "picIsleme";
+            this.picIsleme.Size = new System.Drawing.Size(631, 334);
+            this.picIsleme.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picIsleme.TabIndex = 0;
+            this.picIsleme.TabStop = false;
+            // 
+            // txtMesaj
+            // 
+            this.txtMesaj.Location = new System.Drawing.Point(6, 21);
+            this.txtMesaj.Multiline = true;
+            this.txtMesaj.Name = "txtMesaj";
+            this.txtMesaj.Size = new System.Drawing.Size(340, 122);
+            this.txtMesaj.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtMesaj);
+            this.groupBox2.Location = new System.Drawing.Point(12, 400);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(352, 149);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Gizlenecek Mesaj";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnCoz);
+            this.groupBox3.Controls.Add(this.btnGizle);
+            this.groupBox3.Controls.Add(this.btnKaydet);
+            this.groupBox3.Controls.Add(this.btnAc);
+            this.groupBox3.Location = new System.Drawing.Point(370, 400);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(285, 149);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "İşlemler";
+            // 
+            // btnCoz
+            // 
+            this.btnCoz.Location = new System.Drawing.Point(165, 91);
+            this.btnCoz.Name = "btnCoz";
+            this.btnCoz.Size = new System.Drawing.Size(75, 42);
+            this.btnCoz.TabIndex = 3;
+            this.btnCoz.Text = "Çöz";
+            this.btnCoz.UseVisualStyleBackColor = true;
+            this.btnCoz.Click += new System.EventHandler(this.btnCoz_Click);
+            // 
+            // btnGizle
+            // 
+            this.btnGizle.Location = new System.Drawing.Point(165, 21);
+            this.btnGizle.Name = "btnGizle";
+            this.btnGizle.Size = new System.Drawing.Size(75, 42);
+            this.btnGizle.TabIndex = 2;
+            this.btnGizle.Text = "Gizle";
+            this.btnGizle.UseVisualStyleBackColor = true;
+            this.btnGizle.Click += new System.EventHandler(this.btnGizle_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(55, 91);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 42);
+            this.btnKaydet.TabIndex = 1;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnAc
+            // 
+            this.btnAc.Location = new System.Drawing.Point(55, 21);
+            this.btnAc.Name = "btnAc";
+            this.btnAc.Size = new System.Drawing.Size(75, 42);
+            this.btnAc.TabIndex = 0;
+            this.btnAc.Text = "Aç";
+            this.btnAc.UseVisualStyleBackColor = true;
+            this.btnAc.Click += new System.EventHandler(this.btnAc_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.hakkındaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(667, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuKullanim,
+            this.menuCikis});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // menuKullanim
+            // 
+            this.menuKullanim.Name = "menuKullanim";
+            this.menuKullanim.Size = new System.Drawing.Size(121, 22);
+            this.menuKullanim.Text = "Kullanım";
+            // 
+            // menuCikis
+            // 
+            this.menuCikis.Name = "menuCikis";
+            this.menuCikis.Size = new System.Drawing.Size(121, 22);
+            this.menuCikis.Text = "Çıkış";
+            this.menuCikis.Click += new System.EventHandler(this.menuCikis_Click);
+            // 
+            // hakkındaToolStripMenuItem
+            // 
+            this.hakkındaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuIletisim,
+            this.mneuHakkinda});
+            this.hakkındaToolStripMenuItem.Name = "hakkındaToolStripMenuItem";
+            this.hakkındaToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.hakkındaToolStripMenuItem.Text = "Hakkında";
+            // 
+            // menuIletisim
+            // 
+            this.menuIletisim.Name = "menuIletisim";
+            this.menuIletisim.Size = new System.Drawing.Size(173, 22);
+            this.menuIletisim.Text = "İletişim";
+            this.menuIletisim.Click += new System.EventHandler(this.menuIletisim_Click);
+            // 
+            // mneuHakkinda
+            // 
+            this.mneuHakkinda.Name = "mneuHakkinda";
+            this.mneuHakkinda.Size = new System.Drawing.Size(173, 22);
+            this.mneuHakkinda.Text = "Program Hakkında";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSayi,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 561);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(667, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripSayi
+            // 
+            this.toolStripSayi.Name = "toolStripSayi";
+            this.toolStripSayi.Size = new System.Drawing.Size(13, 17);
+            this.toolStripSayi.Text = "0";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(135, 17);
+            this.toolStripStatusLabel2.Text = "Karakter gizleyebilirsiniz.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 583);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picIsleme)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox picIsleme;
+        private System.Windows.Forms.TextBox txtMesaj;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnCoz;
+        private System.Windows.Forms.Button btnGizle;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnAc;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuKullanim;
+        private System.Windows.Forms.ToolStripMenuItem menuCikis;
+        private System.Windows.Forms.ToolStripMenuItem hakkındaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuIletisim;
+        private System.Windows.Forms.ToolStripMenuItem mneuHakkinda;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripSayi;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
+
---
+++ Steganografi/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Steganografi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Bitmap bmp = null;
+        private string cikarilanYazi = string.Empty;
+        private void btnAc_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Filter = "Resim Dosyaları (*.png,*.bmp,*.jpg)|*.png;*.bmp;*.jpg";
+
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                picIsleme.Image = Image.FromFile(dialog.FileName);
+                btnGizle.Enabled = true;
+            }
+            int lsb1,lsb2;
+            for (int i = 0; i < picIsleme.Height; i++)
+            {
+                for (int j = 0; j < picIsleme.Width; j++)
+                {
+                    lsb1 = picIsleme.Height * picIsleme.Width * 3;
+                    lsb2 = lsb1 / 8;
+                    toolStripSayi.Text = lsb2.ToString();
+                }
+            }
+
+
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save_dialog = new SaveFileDialog();
+            save_dialog.Filter = "Png Image|*.png|Bitmap Image|*.bmp";
+
+            if (save_dialog.ShowDialog() == DialogResult.OK)
+            {
+                switch (save_dialog.FilterIndex)
+                {
+                    case 0:
+                        {
+                            bmp.Save(save_dialog.FileName, ImageFormat.Png);
+                        }
+                        break;
+                    case 1:
+                        {
+                            bmp.Save(save_dialog.FileName, ImageFormat.Bmp);
+                        }
+                        break;
+                }
+
+            }
+        }
+        private void btnGizle_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Girmiyorum Hocam");
+            bmp = (Bitmap)picIsleme.Image;
+            string yazi = txtMesaj.Text;
+            bmp = StegoIslem.yaziGizle(yazi, bmp);
+            MessageBox.Show("İşlendi. Resmi Kaydetmeyi unutmayın!");
+        }
+
+        private void btnCoz_Click(object sender, EventArgs e)
+        {
+            bmp = (Bitmap)picIsleme.Image;
+            string coz = StegoIslem.Coz(bmp);
+            txtMesaj.Text = "";
+            txtMesaj.Text = coz;
+        }
+
+        private void menuCikis_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menuIletisim_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("*omr_kcmn@outlook.com\n*Ömer Kocaman");
+        }
+    }
+}
